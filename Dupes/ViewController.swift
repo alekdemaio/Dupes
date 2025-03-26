c15d9e6fba269e7886f682c45af43f4e13f7126b
Alek DeMaio <34147578+alekdemaio@users.noreply.github.com>
2025-03-25 21:05:40 -0400

    Initial Adjustments
    Photo gallery opens, no logic yet.

--- Dupes/ViewController.swift
+++ Dupes/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import PhotosUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addImageView()
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.editedImage] as? UIImage {
+            imageView.image = image
+        } else if let image = info[.originalImage] as? UIImage {
+            imageView.image = image
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    @IBAction func openGalleryBtn(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true)
+    }
+    
+    func addImageView() {
+        imageView = UIImageView()
+        imageView.layer.cornerRadius = 10
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        view.addSubview (imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0),
+            imageView.centerXAnchor.constraint (equalTo: view.centerXAnchor, constant: 0),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            imageView.widthAnchor.constraint (equalTo: imageView.heightAnchor, multiplier: 1),
+        ])
+    }
 }
 
